28c1ec94dc9d49c2bb1d059fad6e98ce7940df4b
Shmoooji <20103408@usc.edu.ph>
2026-03-25 13:49:59 +0800

    Milestone 2: Core Logic & Edge Cases
     feat: implement core math logic and zero-division handling.

--- app/src/main/java/com/example/calculator_midtermsmock/MainActivity.java
+++ app/src/main/java/com/example/calculator_midtermsmock/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-
+//
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvDisplay;
